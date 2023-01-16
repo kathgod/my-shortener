9e16c3a039ee9ec6e81e65ae9c97589dadcd8fb1
kathgod <kathgod@users.noreply.github.com>
2023-01-16 23:55:47 +0300

    first commit iter5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 	MyHandler "urlshortener/internal/app"
 )
 
 const srError = "Server Error"
-const portNumber = ":8080"
+
+// const portNumber = ":8080"
 
 // MyTest функция для проверки тестирования
 func myTest(n int) int {
@@ -19,6 +20,10 @@ func myTest(n int) int {
 }
 
 func main() {
+	portNumber := os.Getenv("SERVER_ADDRESS")
+	if portNumber == "" {
+		portNumber = "localhost:8080"
+	}
 	mapPost := make(map[string]string)
 	mapGet := make(map[string]string)
 	resP := MyHandler.PostFunc(mapPost, mapGet)
@@ -31,9 +36,9 @@ func main() {
 	rtr.Post("/", resP)
 	rtr.Post("/api/shorten", resPAS)
 	rtr.MethodNotAllowed(resNam)
-	fmt.Printf("Starting application on port %v\n", portNumber)
-	err := http.ListenAndServe(portNumber, rtr)
-	if err != nil {
+	//fmt.Printf("Starting application on port %v\n", portNumber)
+	err2 := http.ListenAndServe(portNumber, rtr)
+	if err2 != nil {
 		log.Println(srError)
 	}
 
